2003f7b8076042cc1148b977d67a70b3b27e84d6
aniko55 <anikowrites@gmail.com>
2024-02-15 20:43:41 +0530

    initial start

---
+++ ochi/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className=' w-full px-20 py-8'>
+      <div className=" logo"></div>
+    </div>
+  )
+}
+
+export default Navbar
